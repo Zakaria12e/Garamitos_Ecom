a3ce7d70e7130d73bd6b3131b8fd0f40ee77dcd8
Zakaria12e <zakariael1212@gmail.com>
2026-03-05 15:27:55 +0000

    feat: add CORS middleware for handling cross-origin requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import { errorHandler, notFound } from './middleware/errorHandler.js'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import rateLimit from 'express-rate-limit'
+import cors from 'cors'
 
 import authRoutes     from './routes/auth.js'
 import productRoutes  from './routes/products.js'
@@ -22,6 +23,14 @@ const PORT = process.env.PORT || 5000
 // Security & Logging
 app.use(helmet())
 app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'combined'))
+
+// CORS
+app.use(
+  cors({
+    origin: [ process.env.CLIENT_URL || 'http://localhost:5173'],
+    credentials: true,
+  })
+)
 
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }))
