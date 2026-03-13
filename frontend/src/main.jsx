f2335d4d2d91e744a8436d4e5788e302fd424214
Zakaria12e <zakariael1212@gmail.com>
2026-03-13 02:17:10 +0000

    feat(main): wrap app with ThemeProvider, AuthProvider, and SettingsProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'next-themes'
+import { AuthProvider } from './context/AuthContext'
+import { SettingsProvider } from './context/SettingsContext'
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} storageKey="techvault-theme">
+        <AuthProvider>
+           <SettingsProvider>
+            <App />
+          </SettingsProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
